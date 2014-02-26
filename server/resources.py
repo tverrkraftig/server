b7903150a4b4077040547dd043fb9164ea03f768
Emil Taylor Bye <emil@wodinaz.com>
2014-02-26 10:23:23 +0100

    Work from previous week and a fix

--- server/resources.py
+++ server/resources.py
@@ -2,7 +2,7 @@ from flask import request
 from flask.ext import restful
 from pymongo import MongoClient
 from tools.decorators import get_str_object_or_404
-from tools.helpers import get_microtime
+from tools.helpers import get_microtime, unicode_to_str
 
 mongodb = MongoClient().db
 
@@ -20,48 +20,82 @@ class Status(restful.Resource):
         data["timestamp"] = get_microtime()
 
         self.collection.update({'device_id': id}, data, upsert=True)
-        # TODO Return get commands
-        return {}
+
+        return {"commands": Command().get(id)}
 
 class Data(restful.Resource):
     def __init__(self):
         self.collection = mongodb.data
 
     @get_str_object_or_404
-    def get(self, id, sensor=None):
-        if sensor != None:
-            return self.collection.find_one({'device_id': id, 'sensor': sensor})
-        else:
-            return [sensor for sensor in self.collection.find({'device_id': id})]
+    def get(self, id, sensor):
+        return self.collection.find_one({'device_id': id, 'sensor': sensor})
 
-    def post(self, id, sensor=None):
+    def post(self, id, sensor):
         data = request.get_json(force=True, cache=False)
 
-        if sensor == None:
-            for sensor_data in data:
-                sensor_data["device_id"] = id
-                sensor_data["timestamp"] = get_microtime()
-                self.collection.update({'device_id': id, 'sensor': sensor_data['sensor']}, sensor_data, upsert=True)
-        else:
-            data["device_id"] = id
-            data["timestamp"] = get_microtime()
-            self.collection.update({'device_id': id, 'sensor': sensor}, data, upsert=True)
+        data["device_id"] = id
+        data["timestamp"] = get_microtime()
+        data["sensor"] = sensor
+        self.collection.update({'device_id': id, 'sensor': sensor}, data, upsert=True)
 
-        # TODO Return get commands
-        return {}
+        return {"commands": Command().get(id)}
 
-class Command(restful.Resource):
+class Data_Collection(restful.Resource):
     def __init__(self):
-        self.collection = mongodb.commands
+        self.collection = mongodb.data
 
+    @get_str_object_or_404
     def get(self, id):
-        # TODO Write this
-        pass
+        return [sensor for sensor in self.collection.find({'device_id': id})]
 
     def post(self, id):
         data = request.get_json(force=True, cache=False)
-        data["device_id"] = id
-        data["timestamp"] = get_microtime()
-        self.collection.insert(data)
-        # TODO return something?
+
+        for sensor_data in data:
+            sensor_data["device_id"] = id
+            sensor_data["timestamp"] = get_microtime()
+            self.collection.update({'device_id': id, 'sensor': sensor_data['sensor']}, sensor_data, upsert=True)
+
+        return {"commands": Command().get(id)}
+
+#TODO finally-something to fix lockups
+class Command(restful.Resource):
+    def __init__(self):
+        self.collection = mongodb.commands
+        # TODO rename for readability?
+        self.id = hex(id(self))
+
+    def __get_document_lock(self, id):
+        # TODO magic happens here, this creates a document if it does not exist
+        document = self.collection.find_one({"device_id": id})
+
+        if not document:
+            self.collection.insert({"device_id": id, "state": self.id, "queue": []})
+            document = self.collection.find_one({"device_id": id})
+
+        while document["state"] != self.id:
+            while document["state"] != "ready":
+                document = self.collection.find_one({"device_id": id})
+            self.collection.update({"device_id": id, "state": "ready"}, {"$set": {"state": self.id}})
+            document = self.collection.find_one({"device_id": id})
+
+    def __free_document_lock(self, id):
+        self.collection.update({"device_id": id}, {"$set": {"state": "ready"}})
+
+    def get(self, id):
+        self.__get_document_lock(id)
+        document = self.collection.find_one({"device_id": id})
+        self.collection.update({"device_id": id}, {"$set": {"queue": []}})
+        self.__free_document_lock(id)
+
+        return unicode_to_str(document["queue"])
+
+    def post(self, id):
+        command = request.get_json(force=True, cache=False)
+        command["timestamp"] = get_microtime()
+
+        self.__get_document_lock(id)
+        self.collection.update({"device_id": id}, {"$push": {"queue": command}})
+        self.__free_document_lock(id)
         return {}
--- server/server.py
+++ server/server.py
@@ -5,27 +5,9 @@ import resources
 app = Flask(__name__)
 api = restful.Api(app)
 
-"""
-class MongoWorld(restful.Resource):
-    @get_str_object_or_404
-    def get(self, id):
-        return collection.find_one({'mongo_id': id})
-
-    def put(self, id):
-        collection.update({'mongo_id': id}, {'mongo_id': id, 'data': request.form['data']}, upsert=True)
-        return self.get(id)
-
-    def post(self, id):
-        self.put(id)
-
-    def delete(self, id):
-        return collection.remove({'mongo_id': id})
-
-api.add_resource(MongoWorld, '/<string:id>')"""
-
 api.add_resource(resources.Status, '/status/<string:id>')
-api.add_resource(resources.Data, '/data/<string:id>', '/data/<string:id>/<string:sensor>', defaults={'sensor':None})
-#api.add_resource(resources.Data, '/data/<string:id>/<string:sensor>')
+api.add_resource(resources.Data, '/data/<string:id>/<string:sensor>')
+api.add_resource(resources.Data_Collection, '/data/<string:id>')
 api.add_resource(resources.Command, '/command/<string:id>')
 
 if __name__ == '__main__':
