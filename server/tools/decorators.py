e5675a79f7ece3bf4e7345223e7a4d019c5173be
Odd <oddweb42+gith@gmail.com>
2014-03-12 15:02:37 +0100

    uuhhhhhhh

--- server/tools/decorators.py
+++ server/tools/decorators.py
@@ -6,8 +6,22 @@ def get_str_object_or_404(action):
     @wraps(action)
     def wrapper(*args, **kwargs):
         result = action(*args, **kwargs)
-        if not result:
+		if not result:
             return Response(response = {}, status = 404, mimetype = "application/json")
         else:
             return Response(response = unicode_to_str(result), mimetype = "application/json")
     return wrapper
+
+def verify_signature(request):
+	def real_wrapper(action):
+		@wraps(action)
+		def wrapper(*args, **kwargs):
+			userAllowed 
+			if not userAllowed:
+				# request should be denied
+				# raise exception
+				pass
+			else:
+				return Response(response = unicode_to_str(result), mimetype = "application/json")
+		return wrapper
+	return real_wrapper
---
+++ server/tools/verification.py
@@ -0,0 +1,3 @@
+def verify_signature(message):
+# do stuff
+	return result
