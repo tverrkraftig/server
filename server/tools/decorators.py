24f4db2ec9a5f5f6825d91e33271e31632393e5d
Odd <oddweb42+gith@gmail.com>
2014-03-19 10:06:10 +0100

    well it's sort of fleshed out now, gotta import the lib and figure out with algorithm to use

--- server/tools/decorators.py
+++ server/tools/decorators.py
@@ -1,6 +1,7 @@
 from flask import Response
 from functools import wraps
 from helpers import unicode_to_str
+# gotta import the pycrypto stuff
 
 def get_str_object_or_404(action):
     @wraps(action)
@@ -16,12 +17,23 @@ def verify_signature(request):
 	def real_wrapper(action):
 		@wraps(action)
 		def wrapper(*args, **kwargs):
-			userAllowed 
+# putt koden din her Odd
+			# get the JSON as a dictionary
+        	data = request.get_json(force=True, cache=False)
+			userID = data["userID"]
+			signature = data["signature"]
+			json = data["body"]
+			# some function that gets the public key associated with this ID
+			key = get_key_for_user(userID)
+			h = SHA.new(json)
+			verifier = PKCS1_v1_5.new(key)
+			userAllowed = verifier.verify(h, signature)
 			if not userAllowed:
 				# request should be denied
 				# raise exception
 				pass
 			else:
+				# I think this should return the body or whatever instead but idk
 				return Response(response = unicode_to_str(result), mimetype = "application/json")
 		return wrapper
 	return real_wrapper
