6ffe8ddd90cf0ec6e3d54af29376073e28f9c288
Emil Taylor Bye <emil@wodinaz.com>
2014-03-19 11:09:14 +0100

    Allow cross domain access

--- server/tools/decorators.py
+++ server/tools/decorators.py
@@ -7,7 +7,7 @@ def get_str_object_or_404(action):
     def wrapper(*args, **kwargs):
         result = action(*args, **kwargs)
         if not result:
-            return {}, 404
+            return {}, 404, {'Access-Control-Allow-Origin': '*'}
         else:
-            return unicode_to_str(result)
+            return unicode_to_str(result), 200, {'Access-Control-Allow-Origin': '*'}
     return wrapper
